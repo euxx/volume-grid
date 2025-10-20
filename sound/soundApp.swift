65e0415be70e76999fb50c50e08a4e706c0ab8d8
eux <euxx@users.noreply.github.com>
2025-10-20 15:42:55 +0800

    Initial Commit

---
+++ sound/soundApp.swift
@@ -0,0 +1,17 @@
+//
+//  soundApp.swift
+//  sound
+//
+//  Created by Eli on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct soundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

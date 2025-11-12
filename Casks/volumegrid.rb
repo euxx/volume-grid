41ec71673c3f3808be0b176600696cebb8494b60
eux <euxx@users.noreply.github.com>
2025-11-12 11:16:01 +0800

    feat: add initial Homebrew Cask definition for VolumeGrid

---
+++ Casks/volumegrid.rb
@@ -0,0 +1,15 @@
+cask "volumegrid" do
+  version "0.1.0"
+  sha256 "PLACEHOLDER_SHA256_HASH"
+
+  url "https://github.com/euxx/VolumeGrid/releases/download/v#{version}/VolumeGrid-#{version}.dmg"
+  name "VolumeGrid"
+  desc "A volume control HUD for macOS"
+  homepage "https://github.com/euxx/VolumeGrid"
+
+  depends_on macos: ">= :ventura"
+
+  app "VolumeGrid.app"
+
+  uninstall quit: "eux.volumegrid"
+end
